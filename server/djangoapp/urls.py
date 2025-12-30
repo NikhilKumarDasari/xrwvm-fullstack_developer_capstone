2ddad644defee582bc1a574242588ef3e464beb1
NikhilKumarDasari <nickydasari@gmail.com>
2025-12-30 11:07:18 -0500

    Add Dealer Page & reviews

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -16,7 +16,7 @@ urlpatterns = [
 
     path(route='get_cars', view=views.get_cars, name ='getcars'),
     # path for dealer reviews view
-    path('get_dealers', views.get_dealerships, name='get_dealers'),
+    path('get_dealers/', views.get_dealerships, name='get_dealers'),
     path('get_dealers/<str:state>', views.get_dealerships, name='get_dealers_by_state'),
     path('dealer/<int:dealer_id>', views.get_dealer_details, name='dealer_details'),
     path('reviews/dealer/<int:dealer_id>', views.get_dealer_reviews, name='dealer_details'),
