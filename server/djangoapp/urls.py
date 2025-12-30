b3343705303cec81f8b43ba2cf2dee51acf35b07
NikhilKumarDasari <nickydasari@gmail.com>
2025-12-30 09:54:04 -0500

    Function to Interact with Backend

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -16,7 +16,11 @@ urlpatterns = [
 
     path(route='get_cars', view=views.get_cars, name ='getcars'),
     # path for dealer reviews view
-
+    path('get_dealers', views.get_dealerships, name='get_dealers'),
+    path('get_dealers/<str:state>', views.get_dealerships, name='get_dealers_by_state'),
+    path('dealer/<int:dealer_id>', views.get_dealer_details, name='dealer_details'),
+    path('reviews/dealer/<int:dealer_id>', views.get_dealer_reviews, name='dealer_details'),
     # path for add a review view
+    path(route='add_review', view=views.add_review, name='add_review'),
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
